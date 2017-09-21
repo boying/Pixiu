1f79ac0fcd36834abfa91f5c566fa812c5848bd3
boying <boyinnju@gmail.com>
2017-09-21 10:54:35 +0800

    add mockito sample

---
+++ test/src/main/java/jzw/MockitoSample.java
@@ -0,0 +1,28 @@
+package jzw;
+
+import org.mockito.Mockito;
+
+import java.util.LinkedList;
+
+/**
+ * Created by jiangzhiwen592 on 2017/9/21.
+ */
+public class MockitoSample {
+    public static void main(String[] args) {
+        smaple1();
+    }
+
+    static void smaple1() {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = Mockito.mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        Mockito.when(mockedList.get(0)).thenReturn("first");
+
+        // the following prints "first"
+        System.out.println(mockedList.get(0));
+
+        // the following prints "null" because get(999) was not stubbed
+        System.out.println(mockedList.get(999));
+    }
+}
