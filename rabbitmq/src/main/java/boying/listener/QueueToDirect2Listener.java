119f95ea53be2ebf31c686311816fd7dc99bda1e
boying <boyinnju@gmail.com>
2017-11-26 21:28:06 +0800

    add manual ack sample

--- rabbitmq/src/main/java/boying/listener/QueueToDirect2Listener.java
+++ rabbitmq/src/main/java/boying/listener/QueueToDirect2Listener.java
@@ -1,22 +1,33 @@
 package boying.listener;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 
 import java.io.UnsupportedEncodingException;
 
 /**
  * Created by boying on 2017/9/29.
  */
-public class QueueToDirect2Listener implements MessageListener{
+public class QueueToDirect2Listener implements ChannelAwareMessageListener {
     @Override
-    public void onMessage(Message message) {
+    public void onMessage(Message message, Channel channel) throws Exception {
+        System.out.println("####################QueueToDirect2Listener###############");
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        if(true)
+        throw new RuntimeException("xx");
+        try {
+            //Thread.sleep(1000000000000L);
+        }catch (Throwable t){}
+
+        if (false)
+            throw new RuntimeException("hahaha");
         try {
             String msgBody = new String(message.getBody(), "UTF-8");
             System.out.println("queue_to_direct 2 listener recv: " + msgBody);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
     }
 }
